35e070c77270745a8cdaba201a7d3e3724988253
WrappedUsername <wrappedusername@gmail.com>
2023-06-13 10:26:35 -0600

    Configuring files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex-col min-h-screen items-center justify-between">
-      <h1 className="flex h-24 bg-gradient-to-r from-green-400 to-purple-600 w-full text-4xl font-bold float-left .h1">Home</h1>
-      <video autoPlay muted loop className="flex object-cover .video">
-        <source src="/vbg.mp4" type="video/mp4" />
+    <main className='relative min-h-screen flex-col items-center justify-between'>
+      <h1 className='absolute h-24 w-full flex-row text-normal font-normal text-white text-center bottom-0 animate-bounce .h1'>Please, scroll down</h1>
+      <video autoPlay muted loop className='flex object-cover .video'>
+        <source src='/vbg.mp4' type='video/mp4' />
       </video>
+      <div className='flex h-screen w-flex-fit text-4xl font-bold'>
+        Home
+        </div>
     </main>
   );
 }

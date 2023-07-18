4fec418e12bfe69a10a12270ee9ae4eff9cd71e6
WrappedUsername <wrappedusername@gmail.com>
2023-07-18 09:00:39 -0500

    adding navbar with broken leg

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import Link from "next/link";
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
-    <nav className="relative">
-    <Link className="fixed h-24 flex-row text-2xl font-bold cursor-pointer m-2" href={'/gallery/'}>Gallery</Link>
+    <nav className='relative'>
+      <div className="flex flex-col md:flex-row h-24 w-full justify-between p-2">
+        <Link 
+          className='fixed h-24 cursor-pointer text-2xl font-bold'
+          href={"/"}
+        >
+          Pickle Monster Cinematics
+        </Link>
+        </div>
+        <div className="flex flex-col md:flex-row h-24 w-full justify-between p-2">
+        <Link
+          className='fixed h-24 cursor-pointer text-2xl font-bold float-right'
+          href={"/gallery/"}
+        >
+          Gallery
+        </Link>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

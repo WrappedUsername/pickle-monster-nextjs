ae42d3b746b111513aa1e3f7ebb30f5779a267eb
WrappedUsername <wrappedusername@gmail.com>
2023-07-22 07:41:54 -0500

    adding navbar

--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -15,24 +15,20 @@ const Navbar: React.FC = () => {
 []);
 
   return (
-    <nav className='relative'>
+    <nav className='relative h-16 w-screen'>
       <div className='flexBetween'>
-      <div className="flex h-24 w-3/4 p-2">
         <Link 
-          className='fixed h-24 w-3/4 cursor-pointer text-2xl font-bold'
+          className='flex flex-col md:flex-row h-16 w-3/4 cursor-pointer text-2xl font-bold'
           href={"/"}
         >
           Pickle Monster Cinematics
         </Link>
-        </div>
-        <div className="flex h-24 w-1/4 p-2">
         <Link
-          className='fixed h-24 w-1/4 cursor-pointer text-2xl font-bold float-right'
+          className='flex flex-col md:flex-row h-16 w-1/4 cursor-pointer text-2xl font-bold float-right'
           href={"/gallery/"}
         >
           Gallery
         </Link>
-      </div>
       </div>
     </nav>
   );

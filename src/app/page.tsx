b4c588dbb579ee40d0ff2794a5f7cd85e4d98db0
WrappedUsername <wrappedusername@gmail.com>
2023-07-24 06:30:58 -0500

    adding navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='flex flex-col w-full h-screen items-center justify-between p-12 pt-64'>
+    <div className='flex flex-col w-full h-screen items-center justify-between p-12 pt-64 md:pt-12'>
       {/**<video autoPlay muted loop className='.video flex object-cover'>
         <source src='/vbg.mp4' type='video/mp4' />
   </video>*/}

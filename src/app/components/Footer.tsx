294ca5ab650b73887ac1486f35a2e07d0d632f2c
WrappedUsername <wrappedusername@gmail.com>
2023-07-24 09:30:41 -0500

    Adding footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className='flex justify-between text-center text-4xl font-bold'>
+    <footer className='relative w-full h-16'>
+        <div className='flexBetween'>
       <Link
-        className='flex h-32 w-full cursor-pointer flex-col p-4 text-2xl font-bold md:h-16 md:w-3/4 md:flex-row'
+        className='flex h-16 w-full cursor-pointer flex-col p-4 text-2xl font-bold md:h-16 md:w-1/2 md:flex-row'
         href={'/'}
       >
         Pickle Monster Cinematics
       </Link>
+      <p className="flex flex-col md:flex-row h-16 w-full md:1/2 text-sm justify-center items-center p-4">
+          &copy; {new Date().getFullYear()} Pickle Monster Cinematics. All rights
+          reserved.
+        </p>
+        </div>
     </footer>
   );
 };

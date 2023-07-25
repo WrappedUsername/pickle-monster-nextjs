acfa13c0aafd35097b2e3ec0b37adb8b1defbde0
WrappedUsername <wrappedusername@gmail.com>
2023-07-25 06:23:05 -0500

    Adding footer

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
 import Link from 'next/link';
+import {FaDiscord, FaGithub} from 'react-icons/fa';
+import {discordLink, githubLink} from '../context/constants';
 
 const Footer = () => {
   return (
-    <footer className='relative w-full h-16'>
-        <div className='flexBetween'>
-      <Link
-        className='flex h-16 w-full cursor-pointer flex-col p-4 text-2xl font-bold md:h-16 md:w-1/2 md:flex-row'
-        href={'/'}
-      >
-        Pickle Monster Cinematics
-      </Link>
-      <p className="flex flex-col md:flex-row h-16 w-full md:1/2 text-sm justify-center items-center p-4">
-          &copy; {new Date().getFullYear()} Pickle Monster Cinematics. All rights
-          reserved.
+    <footer className='relative h-16 w-full'>
+      <div className='flexBetween'>
+        <Link
+          className='flex h-16 w-full cursor-pointer flex-col p-4 text-2xl font-bold md:h-16 md:w-1/2 md:flex-row'
+          href={'/'}
+        >
+          Pickle Monster Cinematics
+        </Link>
+        <Link
+          href={discordLink}
+          className='float-right mx-4 my-4 flex cursor-pointer items-center justify-center text-2xl'
+        >
+          <FaDiscord />
+        </Link>
+        <Link
+          href={githubLink}
+          className='float-right mx-4 my-4 flex cursor-pointer items-center justify-center text-2xl'
+        >
+          <FaGithub />
+        </Link>
+        <p className='md:1/2 flex h-16 w-full flex-col items-center justify-center p-4 text-sm md:flex-row'>
+          &copy; {new Date().getFullYear()} Pickle Monster Cinematics. All
+          rights reserved.
         </p>
-        </div>
+      </div>
     </footer>
   );
 };
---
+++ src/app/context/constants.ts
@@ -0,0 +1,5 @@
+import { Url } from "next/dist/shared/lib/router/router";
+
+export const discordLink:Url = "https://discord.gg/8dq575KbQX";
+
+export const githubLink:Url = "https://github.com/WrappedUsername";

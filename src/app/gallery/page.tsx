215f7c4e759216c6f543ca52fa16f8bc42f10074
WrappedUsername <wrappedusername@gmail.com>
2023-07-26 08:31:55 -0500

    Adding gallery divs

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,8 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import { loadStripe } from '@stripe/stripe-js';
-import { Elements } from "@stripe/react-stripe-js";
-import CheckoutForm from "../components/CheckoutForm";
+'use client';
+import React, {useEffect, useState} from 'react';
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+import CheckoutForm from '../components/CheckoutForm';
 
 // Make sure to call `loadStripe` outside of a component’s render to avoid
 // recreating the `Stripe` object on every render.
@@ -11,7 +11,7 @@ import CheckoutForm from "../components/CheckoutForm";
 );*/
 
 const GalleryPage: React.FC = () => {
-    /** const [art, setArt] = useState<any>([]);
+  /** const [art, setArt] = useState<any>([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     useEffect(() => {
@@ -33,15 +33,15 @@ const GalleryPage: React.FC = () => {
       <h1 className="text-white flex flex-col h-screen w-full text-4xl font-bold text-center justify-between .h1 p-12 pt-64 md:pt-12">
         {art.title}Gallery
       </h1>
-  );*/ 
-  const [clientSecret, setClientSecret] = React.useState("");
+  );*/
+  const [clientSecret, setClientSecret] = React.useState('');
 
   React.useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    fetch('/api/create-payment-intent', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({items: [{id: 'xl-tshirt'}]}),
     })
       .then((res) => res.json())
       .then((data) => setClientSecret(data.clientSecret));
@@ -56,8 +56,18 @@ const GalleryPage: React.FC = () => {
   };
 
   return (
-    <div className="relative w-full h-fit">
-    <div className="flex flex-row w-96 h-96 p-16 bg-slate-300 rounded-lg place-items-center">
+    <div className='relative h-fit w-full'>
+      <div className='flex flex-row h-96 w-96 justify-center rounded-lg bg-slate-300 p-16'>
+        <h1 className='flex flex-col text-normal justify-center text-center font-bold'>
+          Aviation Art
+        </h1>
+      </div>
+
+      <div className='flex flex-row h-96 w-96 justify-center float-right rounded-lg bg-slate-300 p-16'>
+        <h1 className='flex flex-col text-normal justify-center text-center font-bold'>
+          Aviation Art
+        </h1>
+      </div>
       {/** {clientSecret && (
         <Elements options={options} stripe={stripePromise}>
           <CheckoutForm />
@@ -68,7 +78,6 @@ const GalleryPage: React.FC = () => {
           <CheckoutForm />
         </Elements>
       )} */}
-    </div>
     </div>
   );
 };

234d26f8caa336849f2d4ed5565fe637368b106b
WrappedUsername <wrappedusername@gmail.com>
2023-08-17 11:07:55 -0500

    Configuring files

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,21 +1,12 @@
 'use client';
 import React, {useEffect, useState} from 'react';
-import {loadStripe} from '@stripe/stripe-js';
-import {Elements} from '@stripe/react-stripe-js';
-import CheckoutForm from '../components/CheckoutForm';
-
-// Make sure to call `loadStripe` outside of a component’s render to avoid
-// recreating the `Stripe` object on every render.
-const stripePromise:any = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
 
 const GalleryPage: React.FC = () => {
   const [clientSecret, setClientSecret] = React.useState('');
 
   React.useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch('/api/create-checkout-session', {
+    fetch('/checkout_sessions', {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({items: [{id: 'prod_OKlQjZZnR0I39L'}]}),
@@ -36,8 +27,8 @@ const GalleryPage: React.FC = () => {
     <div className='relative h-fit w-full'>
       <div className='md:pt16 flex h-fit w-full flex-col pt-32 md:flex-row'>
         <div className='flex h-64 w-1/2 flex-row justify-center rounded-lg bg-slate-300 md:h-96'>
-          <h1 className='text-normal flex flex-col justify-center text-center font-bold'>
-            <form action='/api/create-checkout-session' method='POST'>
+        <h1 className='text-normal flex flex-col justify-center text-center font-bold'>
+            <form action='/checkout_sessions' method='POST'>
               <button type='submit'>Checkout</button>
             </form>
           </h1>
@@ -46,8 +37,8 @@ const GalleryPage: React.FC = () => {
           Downloadable Digitial Aviation Art
         </div>
         <div className='float-right flex h-64 w-1/2 flex-row justify-center rounded-lg bg-slate-300 md:h-96'>
-          <h1 className='text-normal flex flex-col justify-center text-center font-bold'>
-            <form action='/api/create-checkout-session' method='POST'>
+        <h1 className='text-normal flex flex-col justify-center text-center font-bold'>
+            <form action='/checkout_sessions' method='POST'>
               <button type='submit'>Checkout</button>
             </form>
           </h1>
